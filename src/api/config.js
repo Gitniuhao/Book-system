c6b3d8d0619163b177abd8522435229848a1d17b
niuhao <Niuhao1124@163.com>
2020-12-03 17:11:37 +0800

    feat:修改接口

--- src/api/config.js
+++ src/api/config.js
@@ -2,11 +2,12 @@
  * @Author: 牛皓
  * @Date: 2020-08-10 18:20:30
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 14:10:20
+ * @LastEditTime: 2020-12-03 17:09:47
  * @FilePath: \BookSystem\src\api\config.js
  */
 // export const SERVER = 'http://192.168.10.75:8980'
-export const SERVER =  process.env.NODE_ENV === 'development' ?'http://192.168.10.75:8980' : window.location.origin + '/openbook'
+export const SERVER = window.location.origin + '/openbook'
+// export const SERVER =  process.env.NODE_ENV === 'development' ? 'http://192.168.10.75:8980' : window.location.origin + '/openbook'
 
 export const API_CONFIG = {
 	login:          	 ['/api/rays/login','post'],
--- src/pages/login/store/actionCreator.js
+++ src/pages/login/store/actionCreator.js
@@ -2,7 +2,7 @@
  * @Author: 牛皓
  * @Date: 2020-11-27 10:47:01
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 11:09:55
+ * @LastEditTime: 2020-12-03 17:10:19
  * @FilePath: \BookSystem\src\pages\login\store\actionCreator.js
  */
 //在此页面定义并导出各个action(对象)，便于维护与处理
