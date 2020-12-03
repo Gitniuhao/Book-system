2c39d895df340780c65b7f3417a504ed1b6e1d16
niuhao <Niuhao1124@163.com>
2020-12-03 18:09:16 +0800

    feat:增加路由

--- src/api/config.js
+++ src/api/config.js
@@ -2,12 +2,12 @@
  * @Author: 牛皓
  * @Date: 2020-08-10 18:20:30
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 18:01:31
+ * @LastEditTime: 2020-12-03 18:08:42
  * @FilePath: \BookSystem\src\api\config.js
  */
 // export const SERVER = 'http://192.168.10.75:8980'
 // export const SERVER = window.location.origin + '/openbook'
-export const SERVER =  process.env.NODE_ENV === 'development' ? 'http://192.168.10.75:8980' : window.location.origin +"/raysbook"
+export const SERVER =  process.env.NODE_ENV === 'development' ? 'http://192.168.10.75:8980' : window.location.origin +"/api/bi"
 
 export const API_CONFIG = {
 	login:          	 ['/api/rays/login','post'],
