aac89c9f4f7eb7f9fde36288889bcccb128d1b96
niuhao <Niuhao1124@163.com>
2020-12-03 18:28:46 +0800

    feat:区分一级路由

--- src/api/config.js
+++ src/api/config.js
@@ -2,12 +2,12 @@
  * @Author: 牛皓
  * @Date: 2020-08-10 18:20:30
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 18:19:44
+ * @LastEditTime: 2020-12-03 18:27:24
  * @FilePath: \BookSystem\src\api\config.js
  */
 // export const SERVER = 'http://192.168.10.75:8980'
 // export const SERVER = window.location.origin + '/openbook'
-export const SERVER =  process.env.NODE_ENV === 'development' ? 'http://192.168.10.75:8980' : window.location.origin + '/RaysSystem'
+export const SERVER =  process.env.NODE_ENV === 'development' ? 'http://192.168.10.75:8980' : window.location.origin 
 
 export const API_CONFIG = {
 	login:          	 ['/api/rays/login','post'],
