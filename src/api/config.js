839eb766aa71a8af31aa3f660d9ab58f6821aea9
niuhao <Niuhao1124@163.com>
2020-12-03 16:54:44 +0800

    feat:修改接口api

--- src/api/config.js
+++ src/api/config.js
@@ -2,11 +2,11 @@
  * @Author: 牛皓
  * @Date: 2020-08-10 18:20:30
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 09:23:39
- * @FilePath: \Book-system\src\api\config.js
+ * @LastEditTime: 2020-12-03 14:10:20
+ * @FilePath: \BookSystem\src\api\config.js
  */
-export const SERVER = 'http://192.168.10.75:8980'
-// export const SERVER =  process.env.NODE_ENV === 'development' ?'http://192.168.10.75:8060' : window.location.origin + '/raysbi'
+// export const SERVER = 'http://192.168.10.75:8980'
+export const SERVER =  process.env.NODE_ENV === 'development' ?'http://192.168.10.75:8980' : window.location.origin + '/openbook'
 
 export const API_CONFIG = {
 	login:          	 ['/api/rays/login','post'],
