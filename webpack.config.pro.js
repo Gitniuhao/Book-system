b3c8db6e9e12159c13d57b5b91cd98a6b40a77bb
niuhao <Niuhao1124@163.com>
2020-12-03 18:45:33 +0800

    feat:修改webpack配置

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -2,7 +2,7 @@
  * @Author: 牛皓
  * @Date: 2020-10-27 11:40:28
  * @LastEditors: 牛皓
- * @LastEditTime: 2020-12-03 11:15:02
+ * @LastEditTime: 2020-12-03 18:45:10
  * @FilePath: \BookSystem\webpack.config.pro.js
  */
 const {merge} = require('webpack-merge')
@@ -31,16 +31,16 @@ module.exports = merge(dev,{
 				}
 			  }			
 		}),		
-		// new CompressionWebpackPlugin({//压缩打包体积
-		// 	filename: "[path].gz[query]",  //压缩后的文件策略
-		// 	algorithm: 'gzip',
-		// 	// 需要压缩的资源
-		// 	test:  /\.(js|css|jpg|jpeg|html|ico|png)(\?.*)?$/i, // 开启gzip压缩， 按需写入
-		// 	// 只处理比这个值大的资源。按字节计算
-		// 	threshold: 10240,
-		// 	deleteOriginalAssets: false, // 是否删除原文件
-		// 	// 只有压缩率比这个值小的资源才会被处理
-		// 	minRatio: 0.8
-		// }),
+		new CompressionWebpackPlugin({//压缩打包体积
+			filename: "[path].gz[query]",  //压缩后的文件策略
+			algorithm: 'gzip',
+			// 需要压缩的资源
+			test:  /\.(js|css|jpg|jpeg|html|ico|png)(\?.*)?$/i, // 开启gzip压缩， 按需写入
+			// 只处理比这个值大的资源。按字节计算
+			threshold: 10240,
+			deleteOriginalAssets: false, // 是否删除原文件
+			// 只有压缩率比这个值小的资源才会被处理
+			minRatio: 0.8
+		}),
     ]
 })
