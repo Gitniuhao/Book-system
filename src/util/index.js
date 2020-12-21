9f34e6c2c3c12f7a846e52267825ebcf91846cd5
niuhao <Niuhao1124@163.com>
2020-12-21 17:27:23 +0800

    feat:转换

--- src/util/index.js
+++ src/util/index.js
@@ -192,7 +192,7 @@ export const JudgeWeb = (web)=>{
 	}
 }
 export const TransformUrl = (url)=>{	
-	let Reg =/'.hoguu.com:18000'/ig
+	let Reg = new RegExp('.hoguu.com:18000','ig')
 	if(Reg.test(url)){
 		let newUrl = url.replace(Reg,``)
 		let reg = new RegExp('http://','ig')
